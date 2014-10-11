88cf25ca52bee56af8e6c15b0f28cdc905767365
ritsz <r.ranjan789@gmail.com>
2014-10-11 23:47:00 +0530

    Destroying a container

--- lxc_wrapper.py
+++ lxc_wrapper.py
@@ -5,17 +5,25 @@ import platform
 import sys
 
 BASE_CONTAINER = 'LXC_REMOTE'
+_DEBUG_ = False
+
+
 
 def pr_debug(debug_log) :
-	if len(sys.argv) > 2 and sys.argv[2] == '1' :
+	global _DEBUG_
+	if _DEBUG_ == True :
 		print(debug_log)
+
+
 
 def health_check_stopped(CONTAINER_NAME, container) :
 	assert(container.config_file_name == "%s/%s/config" %(lxc.default_config_path, CONTAINER_NAME))
-	assert(container.init_pid == -1)
+	assert container.init_pid == -1, "Container is already RUNNING"
 	assert(container.name == CONTAINER_NAME)
-	assert(not container.running)
+	assert not container.running, "Container is already RUNNING"
 	assert(container.state == "STOPPED")
+
+
 
 def health_check_started(CONTAINER_NAME, container) :
 	assert(container.config_file_name == "%s/%s/config" %(lxc.default_config_path, CONTAINER_NAME))
@@ -23,6 +31,7 @@ def health_check_started(CONTAINER_NAME, container) :
 	assert(container.name == CONTAINER_NAME)
 	assert(container.running)
 	assert(container.state == "RUNNING")
+
 
 
 def __lxc_create (container):
@@ -39,6 +48,8 @@ def __lxc_create (container):
 	# Return container object 
 	return container
 
+
+
 def lxc_create (container_name) :
 	container = lxc.Container(container_name)
 	if container.defined == True :
@@ -49,17 +60,51 @@ def lxc_create (container_name) :
 	
 	return container
 
+def lxc_kill (container, level = 2) :
+	if container.running :
+		pr_debug("<debug> Stopping the container")
+		container.stop()
+		container.wait("STOPPED", 5)
+		health_check_stopped(container.name, container)
+
+	if level == 2 :
+		pr_debug("<debug> Destroying the container and relinquishing resources")
+		container.destroy()
+		assert(not container.defined)
+
+
 def lxc_start (container) :
 	health_check_stopped(container.name, container);
 	assert container.start(), "Container didn't start"
+	container.wait("RUNNING", 3)
 	pr_debug("<debug> Container has been started")
 	health_check_started(container.name, container)
 	return container
 
-if __name__ == '__main__' :
-	try :
-		container = lxc_create(sys.argv[1])
-	except IndexError :
-		print("Usage : ./lxc_wrapper.py <name of container>")
+
+
+def lxc_attach_process (container, command_str):
+	assert container.running, "Container not running. Process not attached"
+	pid = container.attach(lxc.attach_run_command, command_str.split(" "))
+	return pid
 	
-	container = lxc_start(container)
+
+
+def lxc_main (CONTAINER_NAME, DEBUG) :	
+	global _DEBUG_
+	_DEBUG_ = DEBUG
+	container = lxc_create(CONTAINER_NAME)
+#	container = lxc_start(container)
+	container.attach(lxc.attach_run_command, ["ifconfig", "eth0"])
+	pr_debug(container.get_ips())
+	
+	return container
+
+
+
+
+if __name__ == '__main__' :
+	if (sys.argv[2] == '1') :
+		lxc_main(sys.argv[1], True)
+	else :
+		lxc_main(sys.argv[1], False)
